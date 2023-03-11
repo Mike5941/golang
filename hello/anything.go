4c4a1cf27916c4542bebc1bd1296e115c252791c
wonsoong <mike9159@naver.com>
2023-03-11 15:59:20 +0900

    day 1
    initail commit
    
    morestrings
    
    reverse module
    
    i

---
+++ hello/anything.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello class")
+	foo()
+	fmt.Println("something more")
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+// control flow
+// (1) sequence
+// (2) loop;
+// (3) conditional
