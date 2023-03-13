5abc7405eb329cc28b71832482582412dc056489
mike5941 <mike9159@naver.com>
2023-03-13 17:03:42 +0900

    you're doing great

---
+++ 001-hello/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, _ := fmt.Println("Hello, playground", 42, true)
+	fmt.Println(n)
+}
+
+// control flow
+// (1) sequence
+// (2) loop;
+// (3) conditional
